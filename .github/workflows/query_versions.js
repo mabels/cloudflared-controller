9664cbba3d41c340660e195b81e781bb585d75f2
Meno Abels <meno.abels@adviser.com>
2024-06-06 11:46:17 +0200

    chore: next try

--- .github/workflows/query_versions.js
+++ .github/workflows/query_versions.js
@@ -1,63 +1,29 @@
-const https = require('https');
-
 const APIUSER = process.env.APIUSER && process.env.APIUSER.length ? process.env.APIUSER : undefined;
 
-async function latestRelease(repo, url, page, list, cb) {
-  https.get({
-  hostname: `api.github.com`,
-  port: 443,
-  headers: {
+async function latestRelease(repo, url) {
+  const ghUrl = `https://api.github.com${url}`
+//   console.error(`URL: ${ghUrl}`)
+  const res = await fetch(ghUrl, {
+    headers: {
 	  ...(
 	    typeof(APIUSER) === "string" ? {
 	    	"authorization": `Basic ${Buffer.from(APIUSER).toString('base64')}`,
 	    } : {}
 	  ),
-	  "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+	  // "user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
 	  "accept": "application/vnd.github.v3+json"
-  },
-  path: url,
-  method: 'GET'
-}, (res) => {
-	if (300 < ~~res.statusCode && ~~res.statusCode < 400) {
-		// console.log(res.headers['location']);
-		latestRelease(repo, res.headers['location'])
-		return
-	}
-	if (!(200 <= ~~res.statusCode && ~~res.statusCode < 300)) {
-        	console.error('statusCode:', url, res.statusCode);
-		process.exit(1);
-	}
-	let out = "";
-  	res.on('data', (d) => {
-		out += d;
-	});
-	res.on('end', function (chunk) {
-	    // console.log("# ", url);
-	    console.log(`${repo.toUpperCase().replace(/[^A-Z0-9]/g, '_')}_VERSION=${JSON.parse(out).tag_name}`);
-		/*
-	    const json = JSON.parse(out);
-	    // console.log('res=', json.length, list.map(i => i.tag_name));
-	    if (json.length) {
-		    list.push(...json);
-		    completeList(repo, page+1, list, cb)
-	    } else {
-		    cb(list);
-  	            // console.log(list.map(i => i.name));
-	    }
-	    */
-        });
-  }).on('error', (e) => {
- 	 console.error(e);
-  });
+    }
+  })
+  if (300 < ~~res.status && ~~res.status < 400) {
+	// console.log(res.headers['location']);
+	return latestRelease(repo, res.headers['location'])
+  }
+  if (!(200 <= ~~res.status && ~~res.status < 300)) {
+		console.error('status:', url, res.status);
+  }
+  const out = await res.json()
+  console.log(`${repo.toUpperCase().replace(/[^A-Z0-9]/g, '_')}_VERSION=${out.tag_name}`);
 }
 
-process.argv.slice(2).forEach(repo => {
-  	const url = `/repos/${repo}/releases/latest`;
-	latestRelease(repo, url);
-	/*
-		, versions => {
-		console.log(versions);
-	});
-	*/
-})
+Promise.all(process.argv.slice(2).map(repo => latestRelease(repo, `/repos/${repo}/releases/latest`))).catch(console.error)
 
